706f199102d41b1ed95c8e6f32a43ed034a6ec60
H3-Dolphin <tarezounanoe@gmail.com>
2019-09-15 14:18:32 +0000

    add lot

---
+++ scripts/hello.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = (robot) => {
+    robot.hear(/hello>/i, (msg) => {
+        const username = msg.message.user.name;
+        msg.send('Hello, ' + username);
+    });
+    robot.hear(/'おみくじ'>/i, (msg) => {
+        const username = msg.message.user.name;
+        const lots = ['大吉','吉','中吉','末吉','凶'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(lot + ', ' + username);
+    });
+    robot.hear(/lot>/i, (msg) => {
+        const username = msg.message.user.name;
+        const lots = ['大吉','吉','中吉','末吉','凶'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(lot + ', ' + username);
+    });
+    robot.hear(/'好きだよ'>/i, (msg) => {
+        const username = msg.message.user.name;
+        msg.send('え～えもくない');
+    });
+};
