29afd4bb4648ec0ddf0edf96e197a0f1276972a4
H3-Dolphin <tarezounanoe@gmail.com>
2020-09-02 12:00:54 +0000

    add 0length version

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,27 +4,37 @@ module.exports = (robot) => {
     robot.respond(/todo (.+)/i, (msg) => {
 	const task = msg.match[1].trim();
 	todo.todo(task);
-	msg.send('追加しました: '+ task);
+	msg.send('追加したよ～: '+ task);
     });
 
     robot.respond(/done (.+)/i, (msg) => {
 	const task = msg.match[1].trim();
 	todo.done(task);
-	msg.send('完了にしました: '+ task);
+	msg.send('完了ね～: '+ task);
     });
 
     robot.respond(/del (.+)/i, (msg) => {
 	const task = msg.match[1].trim();
 	todo.del(task);
-	msg.send('削除しました: '+ task);
+	msg.send('削除しといたよ～: '+ task);
     });
 
     robot.respond(/list/i, (msg) => {
-	msg.send(todo.list().join('\n'));
+	const list = todo.list();
+	if(list.length() === 0){
+	   msg.send('予定はないよ～');
+	}else{
+	   msg.send(todo.list().join('\n'));
+	}
     });
 
     robot.respond(/donelist/i, (msg) => {
-	msg.send(todo.donelist().join('\n'));
+	const donelist = todo.donelist();
+	if(donelist.length() === 0){
+	   msg.send('終わった予定はないよ～');
+	}else{
+	   msg.send(todo.donelist().join('\n'));
+	}
     });
 
     robot.hear(/hello>/i, (msg) => {
