bcf310f9209f3cbb9f0c650c801702aa5f6a8842
H3-Dolphin <47548202+H3-Dolphin@users.noreply.github.com>
2020-09-02 21:12:32 +0900

    Update hello.js

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,59 +2,63 @@
 const todo = require('todo');
 module.exports = (robot) => {
     robot.respond(/todo (.+)/i, (msg) => {
-	const task = msg.match[1].trim();
-	todo.todo(task);
-	msg.send('追加したよ～: '+ task);
+	      const task = msg.match[1].trim();
+	      todo.todo(task);
+	      msg.send('追加したよ～: '+ task);
     });
 
     robot.respond(/done (.+)/i, (msg) => {
-	const task = msg.match[1].trim();
-	todo.done(task);
-	msg.send('完了ね～: '+ task);
+	      const task = msg.match[1].trim();
+	      todo.done(task);
+	      msg.send('完了ね～: '+ task);
     });
 
     robot.respond(/del (.+)/i, (msg) => {
-	const task = msg.match[1].trim();
-	todo.del(task);
-	msg.send('削除しといたよ～: '+ task);
+	      const task = msg.match[1].trim();
+	      todo.del(task);
+	      msg.send('削除しといたよ～: '+ task);
     });
 
     robot.respond(/list/i, (msg) => {
-	const list = todo.list();
-	if(list.length === 0){
-	   msg.send('予定はないよ～');
-	}else{
-	   msg.send(list.join('\n'));
-	}
+	      const list = todo.list();
+	      if(list.length === 0){
+	         msg.send('予定はないよ～');
+	      }else{
+	         msg.send(list.join('\n'));
+	      }
     });
 
     robot.respond(/donelist/i, (msg) => {
-	const donelist = todo.donelist();
-	if(donelist.length === 0){
-	   msg.send('終わった予定はないよ～');
-	}else{
-	   msg.send(donelist.join('\n'));
-	}
+	      const donelist = todo.donelist();
+	      if(donelist.length === 0){
+	         msg.send('終わった予定はないよ～');
+	      }else{
+	         msg.send(donelist.join('\n'));
+	      }
     });
 
     robot.hear(/hello>/i, (msg) => {
         const username = msg.message.user.name;
         msg.send('はろー, ' + username);
     });
+  
     robot.hear(/おみくじ/i, (msg) => {
         const username = msg.message.user.name;
         const lots = ['大吉','吉','中吉','末吉','凶'];
         const lot = lots[Math.floor(Math.random() * lots.length)];
         msg.send(lot + 'だよ～ ' + username + 'さん');
     });
+  
     robot.hear(/好きだよ/i, (msg) => {
         const username = msg.message.user.name;
         msg.send('え～えもくない');
     });
+  
     robot.hear(/ちゅー/i, (msg) => {
         const username = msg.message.user.name;
         msg.send('やめてよ～');
     });
+  
     robot.hear(/えもちゃん、(.+)/i, (msg) => {
         const username = msg.message.user.name;
         msg.send('わからないよ～');
